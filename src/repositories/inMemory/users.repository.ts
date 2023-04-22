35b383c19b64769d5f5e1754a171a6ed5cd457ed
Denis Levy de M. Cravo <denis.levi11@gmail.com>
2023-04-22 17:22:34 -0300

    Add: register service test

--- src/repositories/inMemory/users.repository.ts
+++ src/repositories/inMemory/users.repository.ts
@@ -1,20 +1,30 @@
 import { Prisma, User } from '@prisma/client'
 import { IUsersRepository } from '../IUsers.repository'
+import crypto from 'node:crypto'
 
-export class inMemoryUsersRepository implements IUsersRepository {
-  public users: Prisma.UserCreateInput[] = []
+export class InMemoryUsersRepository implements IUsersRepository {
+  public users: User[] = []
 
   async findById(id: string): Promise<User | null> {
     throw new Error('Method not implemented.')
   }
 
   async findByEmail(email: string): Promise<User | null> {
-    throw new Error('Method not implemented.')
+    return this.users.find((user) => user.email === email) || null
   }
 
   async create(data: Prisma.UserCreateInput): Promise<User> {
-    this.users.push(data)
+    const user: User = {
+      id: crypto.randomUUID(),
+      name: data.name,
+      email: data.email,
+      avatar: '',
+      created_at: new Date(),
+      password_hash: data.password_hash,
+    }
 
-    return data as User
+    this.users.push(user)
+
+    return user
   }
 }
--- src/services/register.test.ts
+++ src/services/register.test.ts
@@ -1,24 +1,57 @@
-import { describe, it, expect } from 'vitest'
-import { RegisterUserService } from './registerUser.service'
-import { PrismaUsersRepository } from '@/repositories/prisma/prismaUsers.repository'
+import { describe, it, expect, beforeEach } from 'vitest'
 import { compare } from 'bcryptjs'
+import { RegisterUserService } from './registerUser.service'
+import { InMemoryUsersRepository } from '@/repositories/inMemory/users.repository'
+import { EmailAlreadyExistsError } from './errors/emailAlreadyExists.error'
 
-describe('Register service', () => {
-  it('should create a hash from user password upon registration', async () => {
-    const registerService = new RegisterUserService(new PrismaUsersRepository())
+let registerService: RegisterUserService
 
+describe('Register.service', () => {
+  beforeEach(() => {
+    registerService = new RegisterUserService(new InMemoryUsersRepository())
+  })
+
+  it('should be able to register a user', async () => {
     const { user } = await registerService.execute({
       email: 'test@test.com',
       name: 'test',
       password: '123456',
     })
 
-    console.log(user.password_hash)
-    const isPasswordCurrectlyHashed = await compare(
+    console.log(user)
+
+    expect(user.id).toEqual(expect.any(String))
+  })
+
+  it('should create a hash from user password upon registration', async () => {
+    const { user } = await registerService.execute({
+      email: 'test@test.com',
+      name: 'test',
+      password: '123456',
+    })
+
+    const isPasswordCorrectlyHashed = await compare(
       '123456',
       user.password_hash,
     )
 
-    expect(isPasswordCurrectlyHashed).toEqual(true)
+    expect(isPasswordCorrectlyHashed).toEqual(true)
+  })
+
+  it('should not be able to register with same email twice', async () => {
+    await registerService.execute({
+      email: 'test@test.com',
+      name: 'test',
+      password: '123456',
+    })
+
+    expect(
+      async () =>
+        await registerService.execute({
+          email: 'test@test.com',
+          name: 'test',
+          password: '123456',
+        }),
+    ).rejects.toBeInstanceOf(EmailAlreadyExistsError)
   })
 })
